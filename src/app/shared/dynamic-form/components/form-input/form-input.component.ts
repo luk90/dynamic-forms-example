cf2e7e9971adff550b245d4e7cac5203fe25b4a3
lzoltak <lukasz.zoltak@gmail.com>
2017-05-05 19:38:30 +0200

    added application state logic to validation message component

--- src/app/shared/dynamic-form/components/form-input/form-input.component.ts
+++ src/app/shared/dynamic-form/components/form-input/form-input.component.ts
@@ -1,27 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Field } from '../../model/field';
 import { FieldConfig } from '../../model/field-config';
 import { FormGroup } from '@angular/forms';
+import { ApplicationStateService } from '../../../../main/application/application-state.service';
 
 @Component({
   selector: 'app-form-input',
   templateUrl: './form-input.component.html',
-  // styleUrls: ['./form-input.component.scss'],
-  styles: [`${this.style}`]
+  styleUrls: ['./form-input.component.scss'],
 })
-export class FormInputComponent implements Field {
+export class FormInputComponent implements Field, OnInit {
   config: FieldConfig;
   group: FormGroup;
+  applicationState;
 
-  style = `
- .dynamic-field{
- font-size:20px;
- background-color: red;
-}
-`;
-
-  constructor() {
+  constructor(private applicationStateService: ApplicationStateService) {
   }
 
-
+  ngOnInit(): void {
+    this.applicationState = this.applicationStateService.applicationStateValue;
+  }
 }
